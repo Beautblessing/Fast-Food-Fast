b00855e4eae829037ed7d4ac3ab6ee0f64f38e95
Beautblessing <beautblessing@gmail.com>
2018-09-27 03:54:20 -0700

    bug-fix made corrected the links on the signin and signup pages

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,18 +1,19 @@
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown contents */
 // const dropdown = document.getElementsByClassName('dropdown-btn');
 
-//   for (let i = 0; i < dropdown.length; += 1) {
-//     dropdown[i].addEventListener('click', function() {
-//       this.classList.toggle('active');
-//       let dropdownContent = this.nextElementSibling;
-//       if (dropdownContent.style.display === 'block') {
-//         dropdownContent.style.display = 'none';
-//       } else {
-//         dropdownContent.style.display = 'block';
-//       }
+// for (let i = 0; i < dropdown.length; i++) {
+//     dropdown[i].addEventListener('click', function () {
+//         this.classList.toggle('active');
+//         let dropdownContent = this.nextElementSibling;
+//         if (dropdownContent.style.display === 'block') {
+//             dropdownContent.style.display = 'none';
+//         } else {
+//             dropdownContent.style.display = 'block';
+//         }
 //     });
-//   }
-console.log('welcome');
+// }
+
+
 function redirect(url) {
     location.href = url;
 }
