035a5a3bdbd89c1879fde2a1f7e5e7b71aa4d59d
Beautblessing <Beautblessing@users.noreply.github.com>
2018-09-27 04:13:24 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@ const dropdown = document.getElementsByClassName("dropdown-btn");
 for (let i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
+        const dropdownContent = this.nextElementSibling;
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
         } else {
             dropdownContent.style.display = "block";
         }
     });
+}
+
+function redirect(url) {
+    location.href = url;
 }
 
             
